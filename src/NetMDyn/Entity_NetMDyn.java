015d7c56afac6f20b0eb533292bb4362e4157de7
Emeline Duquenne <eduque910e@emi.u-bordeaux1.fr>
2016-02-12 09:15:55 +0100

    Fin du header et de la JavaDoc

--- src/NetMDyn/Entity_NetMDyn.java
+++ src/NetMDyn/Entity_NetMDyn.java
@@ -1,3 +1,25 @@
+/* This file is part of NetMDyn.
+ *
+ *   NetMDyn is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 3 of the License, or
+ *   any later version.
+ *
+ *   NetMDyn is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with NetBioDyn; if not, write to the Free Software
+ *   Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+/*
+ * Entity_NetMDyn.java
+ *
+ * Created on February 12 2016, 09:07
+ */
+
 package NetMDyn;
 
 import java.awt.image.BufferedImage; // Possibility to create an image with an accessible buffer of image data (a buffered image)
@@ -5,18 +27,31 @@ import java.util.ArrayList; //Possible creation of tables
 import java.util.HashMap; //Possible creation of hashmaps
 import netbiodyn.Entity;
 
+
+/**
+ * Class of Entity management
+ * 
+ * @author Master 2 Bioinformatique
+ */
+
 public class Entity_NetMDyn extends Entity{
 	
 	public String _compartment = "Cytosol";
 	
+	/**
+	 * Initialization of an Entity object
+	 */
     public Entity_NetMDyn() {
         super();
     }
 
-	
-	public Entity_NetMDyn  clone() {
+	/**
+	 * Clone this object
+	 * @return the new Entity
+	 */
+	public Entity_NetMDyn clone() {
         Entity_NetMDyn  reaxel = new Entity_NetMDyn (); // Creation of a new Entity
-        //Retrieval of object parameters
+        // Retrieval of object parameters
         reaxel.setEtiquettes(getEtiquettes());
         reaxel.Couleur=Couleur;
         reaxel._visibleDansPanel=_visibleDansPanel;
@@ -28,19 +63,31 @@ public class Entity_NetMDyn extends Entity{
         if (BackgroundImage != null) {
             reaxel._str_image_deco=_str_image_deco;
         }
-        return reaxel; //Return the new Entity
+        return reaxel; // Return the new Entity
     }
 	
+	/**
+	 * Save the Entity with all its parameters
+	 * @return all saved parameters
+	 */
     public ArrayList<String> toSave() {
-        ArrayList<String> toSave = super.toSave(); //creation of the variable that will contain all informations
+        ArrayList<String> toSave = super.toSave(); // Creation of the variable that will contain all informations
         toSave.add(new String("\tcompartment:").concat(((String) this._compartment).toString()) + "\n");
-        return toSave; //return the table containing all the informations
+        return toSave; // Return the table containing all the informations
     }
     
+    /**
+     * Return the compartment of the Entity
+     * @return the name of the compartment
+     */
 	public String getCompartment() {
 		return _compartment;
 	}
 
+	/**
+	 * Put the Entity into a new compartment
+	 * @param compartment : the new compartment of the Entity
+	 */
 	public void setCompartment(String compartment) {
 		_compartment = compartment;
 	}
