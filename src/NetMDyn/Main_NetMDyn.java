a164b576dc84de8f3f2d49ce0cb78307fdaeb5a0
Emeline Duquenne <eduque910e@emi.u-bordeaux1.fr>
2016-02-12 09:33:27 +0100

    Fin du header et de la JavaDoc

--- src/NetMDyn/Main_NetMDyn.java
+++ src/NetMDyn/Main_NetMDyn.java
@@ -1,12 +1,44 @@
+/* This file is part of NetMDyn.
+ *
+ *   NetMDyn is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 3 of the License, or
+ *   any later version.
+ *
+ *   NetMDyn is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with NetBioDyn; if not, write to the Free Software
+ *   Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+/*
+ * Main_NetMDyn.java
+ *
+ * Created on February 12 2016, 09:28
+ */
+
 package NetMDyn;
 
 import NetMDyn.ihm.Controller_NetMDyn;
+
+/**
+ * Main Class of NetMDyn
+ * 
+ * @author Master 2 Bioinformatique
+ */
 
 public class Main_NetMDyn {
 	//Default size of the software window
 	static int FRAME_WIDTH = 900;
 	static int FRAME_HEIGHT = 600;
-
+	
+	/**
+	 * Opening of NetMDyn
+	 * @param args
+	 */
 	public static void main(String[] args){
 	//The opening of the software causes the creation of the Controller
 	new Controller_NetMDyn();
