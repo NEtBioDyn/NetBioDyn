e8bd56cfbd77e3cb743bb5eaf3a53ea1cd752ebd
Emeline Duquenne <eduque910e@emi.u-bordeaux1.fr>
2016-02-12 14:37:43 +0100

    Rectification coquille header

--- src/NetMDyn/ihm/Controller_NetMDyn.java
+++ src/NetMDyn/ihm/Controller_NetMDyn.java
@@ -1,4 +1,4 @@
-/* This file is part of NetMDyn.
+/* This file is part of NetMDyn.ihm
  *
  *   NetMDyn is free software; you can redistribute it and/or modify
  *   it under the terms of the GNU General Public License as published by
