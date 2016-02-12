7dcefbecd767047298e46946ffc3c39c41fa1192
Emeline Duquenne <eduque910e@emi.u-bordeaux1.fr>
2016-02-12 16:35:17 +0100

    Fin du header et de la JavaDoc

--- src/NetMDyn/util/WriterNbd.java
+++ src/NetMDyn/util/WriterNbd.java
@@ -1,3 +1,25 @@
+/* This file is part of NetMDyn.util
+ *
+ *   NetMDyn is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 3 of the License, or
+ *   any later version.
+ *
+ *   NetMDyn is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with NetBioDyn; if not, write to the Free Software
+ *   Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+/*
+ * WriterNbd.java
+ *
+ * Created on February 12 2016, 16:35
+ */
+
 package NetMDyn.util;
 
 
@@ -19,6 +41,12 @@ import org.sbml.jsbml.SpeciesReference;
 import NetMDyn.SbmlParser;
 import NetMDyn.ihm.MetaboliteVisualizer;
 import netbiodyn.util.Lang;
+
+/**
+ * Writer of nbd files in NetMDyn
+ * 
+ * @author Master 2 Bioinformatique
+ */
 
 public class WriterNbd{
 	private ArrayList<Integer>colorlist= new ArrayList();
